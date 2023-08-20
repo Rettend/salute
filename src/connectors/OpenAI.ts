0b0f3442a19d7e8e77d6fb4ef8f069f77b41f8d3
Hegyi Áron Ferenc <hegyi.aron101@gmail.com>
2023-08-20 17:04:47 +0200

    Revert "first try"
    This reverts commit 8e4a2e21489fb427b8f79b6707b9c33c643643b9.

--- src/connectors/OpenAI.ts
+++ src/connectors/OpenAI.ts
@@ -121,15 +121,15 @@ export const createOpenAIChatCompletion = (
 
 export const gpt3 = createOpenAIChatCompletion(
   { model: "gpt-3.5-turbo" },
-  { apiKey: openAIKey }
+  { apiKey: process.env.OPENAI_KEY }
 );
 
 export const gpt4 = createOpenAIChatCompletion(
   { model: "gpt-4" },
-  { apiKey: openAIKey }
+  { apiKey: process.env.OPENAI_KEY }
 );
 
 export const davinci = createOpenAICompletion(
   { model: "text-davinci-003" },
-  { apiKey: openAIKey }
+  { apiKey: process.env.OPENAI_KEY }
 );
